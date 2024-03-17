d31a9507e1b91f880bb88b738631e5147eff8ade
Nathan <151407945+JuanPabloZed@users.noreply.github.com>
2024-03-17 15:37:16 +0100

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,104 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "AudioFile.h"
+#include <QPushButton>
+#include <QFileDialog>
+#include <QDebug>
+#include <QMessageBox>
+#include <QFileSystemModel>
+
+// define constructor
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    // translates .ui file in actual ui
+    ui->setupUi(this);
+
+    // set filemodel for the treeview
+    QString root = "C:/";
+    dirmodel = new QFileSystemModel(this);
+    dirmodel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    dirmodel->setRootPath(root);
+    ui->treeView->setModel(dirmodel);
+
+    // set filemodel for listview
+    filemodel = new QFileSystemModel(this);
+    filemodel->setRootPath(root);
+    filemodel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
+    filemodel->setNameFilters(QStringList()<<"*.wav");
+    filemodel->setNameFilterDisables(false);
+    ui->files_list->setModel(filemodel);
+
+
+}
+// define destructor
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_createir_button_clicked()
+{
+    // check if everything is ok before computing
+    if (!sweep.isMono()){
+        QMessageBox::critical(this, "Non-mono sweep file", "Sweep file is not mono. Please choose a mono sweep file.");
+        return;
+    }
+
+    // compute
+
+    // now that ir is created, it's possible to play it
+    ui->playir_button->setEnabled(true);
+}
+
+
+void MainWindow::on_browsesweep_button_clicked()
+{
+    // get path of sweep
+    QString sweeppath = QFileDialog::getOpenFileName(this, "Select a sweep file", "C://","WAV files (*.wav)");
+
+    // store sweep data & info
+    sweep.load(sweeppath.toStdString());
+    // check if mono
+    if (!sweep.isMono()){
+        ui->browsesweep_button->setText("Browse sweep file");
+        QMessageBox::critical(this,"Sweep file : mono only", "Mono-only sweep files are supported. Please choose a mono file.");
+        return;
+    }
+
+    // change button text to file name
+    QFileInfo sweepinfo(sweeppath);
+    QString sweepname(sweepinfo.baseName());
+    ui->browsesweep_button->setText(sweepname);
+}
+
+
+void MainWindow::on_treeView_clicked(const QModelIndex &index)
+{
+    QString dirpath = dirmodel->fileInfo(index).absoluteFilePath();
+    ui->files_list->setRootIndex(filemodel->setRootPath(dirpath));
+}
+
+
+
+
+void MainWindow::on_customsave_radio_toggled(bool checked)
+{
+    if (ui->customsave_radio->isChecked()){
+        ui->browseout_button->setEnabled(checked);
+    } else {
+        ui->browseout_button->setEnabled(checked);
+    }
+}
+
+
+void MainWindow::on_autosr_check_stateChanged(int arg1)
+{
+    if (ui->autosr_check->isChecked()){
+        ui->srate->setEnabled(false);
+    } else {
+        ui->srate->setEnabled(true);
+    }
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "AudioFile.h"
+//#include "pffft.h"
+#include <QMainWindow>
+#include <QFileSystemModel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    // construcor, destructor
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    //info for processing
+    QString sweeppath;
+    QString recorddirpath;
+    QString savepathauto; // for autosave
+    QString savepathcstm; // for custom save
+
+    // audio data
+    AudioFile<int> sweep;
+    AudioFile<int> out;
+
+private slots:
+    void on_createir_button_clicked();
+    void on_browsesweep_button_clicked();
+//    void on_loadfolder_button_clicked();
+
+    void on_treeView_clicked(const QModelIndex &index);
+
+
+    void on_customsave_radio_toggled(bool checked);
+
+    void on_autosr_check_stateChanged(int arg1);
+
+private:
+    Ui::MainWindow *ui;
+    QFileSystemModel *filemodel;
+    QFileSystemModel *dirmodel;
+};
+#endif // MAINWINDOW_H
